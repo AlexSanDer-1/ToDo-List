d7b402b397748eb4262b073ec5d2b19f29bbfa6f
AlexSanDer-1 <odhsanderspindola@gmail.com>
2025-06-10 21:23:17 -0300

    Implementa tratamento de erro 404 para recurso não encontrado

---
+++ src/main/java/sander/ToDo_List/infra/RestExceptionHandler.java
@@ -0,0 +1,18 @@
+package sander.ToDo_List.infra;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+  @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<RestErrorMessage> tratarError404(EntityNotFoundException ex){
+          RestErrorMessage  capturaError = new RestErrorMessage(HttpStatus.NOT_FOUND,ex.getMessage());
+        return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(capturaError);
+    }
+}
